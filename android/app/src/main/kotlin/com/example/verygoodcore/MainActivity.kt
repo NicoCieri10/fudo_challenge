722cc774a6bda53c5a11fcc47d944f1ef123b4c8
Nico <102190013+NicoCieri10@users.noreply.github.com>
2023-11-15 23:40:56 -0300

    big bang

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.fudo_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:fudo_challenge/app/app.dart';
+import 'package:fudo_challenge/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
